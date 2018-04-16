643b409bc563c40aa1d100b96bfd52d759324623
Adrià Ferré <aferre@newrelic.com>
2018-04-16 22:08:59 +0200

    style: use hackernews style on index page

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -1,13 +1,12 @@
 class Contribution < ApplicationRecord
-    #belongs_to :user
-
-    validate :text_xor_url
+  belongs_to :user
+  validate :text_xor_url
 
   private
 
-    def text_xor_url
-      unless text.blank? ^ url.blank?
-        errors.add(:base, "Specify a text or a url, not both")
-      end
+  def text_xor_url
+    unless text.blank? ^ url.blank?
+      errors.add(:base, "Specify a text or a url, not both")
     end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180416065934) do
+ActiveRecord::Schema.define(version: 20180417062918) do
 
   create_table "contributions", force: :cascade do |t|
     t.string "title"
     t.string "url"
     t.text "text"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -23,6 +24,7 @@ ActiveRecord::Schema.define(version: 20180416065934) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

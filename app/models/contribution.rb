901e47f2efdd0b6f0845e3ed4013431ee63e8c9d
Adrià Ferré <aferre@newrelic.com>
2018-04-17 08:52:20 +0200

    models/contribution: add valitation to check if url is valid

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -1,12 +1,21 @@
 class Contribution < ApplicationRecord
   belongs_to :user
   validate :text_xor_url
+  validate :url_is_valid
 
   private
 
   def text_xor_url
     unless text.blank? ^ url.blank?
       errors.add(:base, "Specify a text or a url, not both")
+    end
+  end
+
+  def url_is_valid
+    unless url.blank?
+      unless url =~ /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})/
+        errors.add(:base, "URL must be valid")
+      end
     end
   end
 end

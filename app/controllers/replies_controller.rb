06448dc4f4bdfa4a6b558b8e943f46082aaa7abc
Albert Figuera Perez <albert.figue5@gmail.com>
2018-04-27 13:56:38 +0200

    Moldel Reply created, replies_controller created, model/replies folder created

---
+++ app/controllers/replies_controller.rb
@@ -0,0 +1,25 @@
+class RepliesController < ApplicationController 
+  
+  # GET /replies
+  # GET /replies.json
+  def index
+    @replies = Reply.all
+  end
+
+  # GET /replies/1
+  # GET /replies/1.json
+  def show
+  end
+
+  # GET /replies/new
+  def new
+    @reply = Reply.new
+  end
+
+  # GET /replies/1/edit
+  def edit
+  end
+
+
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180427061706) do
+ActiveRecord::Schema.define(version: 20180427114233) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 20180427061706) do
     t.string "url"
     t.text "text"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "replies", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.integer "comment_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

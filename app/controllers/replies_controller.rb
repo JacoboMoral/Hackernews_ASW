f86762d120e6ff0aca2d3e4195f223811d45ca06
Albert Figuera Perez <albert.figue5@gmail.com>
2018-04-27 15:20:47 +0200

    model/replies added, replies_controller updated

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -20,6 +20,39 @@ class RepliesController < ApplicationController
   def edit
   end
 
+  def create
 
+  end
+
+
+  # PATCH/PUT /replies/1
+  # PATCH/PUT /replies/1.json
+  def update
+    respond_to do |format|
+      if @reply.update(reply_params)
+        format.html { redirect_to @reply, notice: 'Reply was successfully updated.' }
+        format.json { render :show, status: :ok, location: @reply }
+      else
+        format.html { render :edit }
+        format.json { render json: @reply.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /replies/1
+  # DELETE /replies/1.json
+  def destroy
+    @reply.destroy
+    respond_to do |format|
+      format.html { redirect_to replies_url, notice: 'Reply was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def reply_params
+      params.require(:reply).permit(:content, :user_id, :comment_id)
+    end
 
 end

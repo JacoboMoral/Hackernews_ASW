d642fc622f861699a3c1aba52ee0344e01b994b4
oriolborrellroig <oriolborrellroig@gmail.com>
2018-04-28 17:02:15 +0200

    Fix error

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'logout', to: 'sessions#destroy'
-  get 'user' => 'users#edit'
+  get 'users' => 'users#edit'
 
   get 'newest' => 'contributions#newest'
   get 'ask' => 'contributions#ask'

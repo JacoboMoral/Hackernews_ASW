927d9f92fa159949034eb1067de54fd08f84a82c
Jacobo Moral Buendía <jacobo.moral@gmail.com>
2018-05-02 20:05:42 +0200

    more style changes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get 'submit' => 'contributions#new'
   get 'comments/:id/' => 'comments#newReply'
 
-  resources :replies
+  resources :replies, only: :create
   resources :comments, only: :create
   resources :users
   resources :contributions, path: '/'
